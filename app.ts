599f119ebbed8a294bd3177f4bf49173444d7107
LeJohne <johngoy11024@gmail.com>
2023-05-19 14:49:45 -0400

    Working with Enums - 6

--- app.ts
+++ app.ts
@@ -2,22 +2,31 @@
 //     name: string;
 //     age: number;
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
-    name: 'John',
-    age: 19,
-    hobbies: ['Coding', 'Photography'],
-    role: [2, 'author']
-};
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: 'John',
+//     age: 19,
+//     hobbies: ['Coding', 'Photography'],
+//     role: [2, 'author']
+// };
+
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
+const person = {
+        name: 'John',
+        age: 19,
+        hobbies: ['Coding', 'Photography'],
+        role: Role.ADMIN
+    };
 
 // person.role.push('admin')
-// person.role[1] = 10;
+// person.role[1] = 10; 
 
-person.role= [0,'admin'];
+// person.role= [0,'admin'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -26,4 +35,8 @@ console.log(person.name);
 
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase())
+}
+
+if(person.role === Role.AUTHOR){
+    console.log('is author')
 }
